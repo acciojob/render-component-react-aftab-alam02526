2d6d749b342b768097902cd5eeb401ace1ec6d15
aftab-alam02526 <aftab0447@gmail.com>
2025-11-02 20:30:50 +0530

    Create index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Line () {
+  return (
+   <div>
+      <p>"I am learning React. My life is getting better."</p>
+   </div>
+  );
+}
+
+export default Line;
